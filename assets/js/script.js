bbac49b7d47606c13c90bd050f85f2c10afe900a
Cesar Martinez <cesaguma@hotmail.com>
2020-09-13 21:40:05 -0400

    Initializing variables into script.js file for DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var boxContainerElem = document.querySelector("#boxContainer");
+var questionElem = document.querySelector("#question");
+var optionsElem = document.querySelector("#options");
+var statusElem = document.querySelector("#status");
+
