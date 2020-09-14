e5c3a668961c0f1215893554199b92c3727851bd
Cesar Martinez <cesaguma@hotmail.com>
2020-09-14 01:47:23 -0400

    Adding styles to the elements in the index.html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-var boxContainerElem = document.querySelector("#boxContainer");
-var questionElem = document.querySelector("#question");
-var optionsElem = document.querySelector("#options");
-var statusElem = document.querySelector("#status");
-var mainElem = document.querySelector("#main");
+
+// Creating variables to control DOM elements   
+var mainElem = document.querySelector("#main");    
+var card = document.createElement("div");
+var cardHeader = document.createElement("div");
+var cardBody = document.createElement("div");
+var cardTitle = document.createElement("h5");
+var cardText = document.createElement("p");
+var startButton = document.createElement("button");
+var timerElem = document.querySelector("#timer");
+
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -16,41 +22,12 @@ var questions = [
 
 var numQuestions = questions.length;
 var cont = 0;
-
-function generateQuestion(){
-    if (cont )
-    for (i in questions) {
-        var titleQuestion = document.createElement("h2");
-        var box = document.createElement("div");
-        console.log(questions[i].title);
-        titleQuestion.textContent = questions[i].title;
-        boxContainerElem.appendChild(titleQuestion);
-        boxContainerElem.appendChild(box);
-        for (j in questions[i].choices) {
-            var listOption = document.createElement("li");  
-            listOption.textContent = questions[i].choices[j];
-            listOption.setAttribute("data-index",j);
-            optionsElem.appendChild(listOption);
-            console.log(questions[i].choices[j]);  
-        }
- 
-      }
-
-}
+var totalSeconds = numQuestions * 20;
+var interval;
 
 function startQuiz(){ // To generate the first display for start the Quiz
  
-// Creating variables to control elements    
-    
-var card = document.createElement("div");
-var cardHeader = document.createElement("div");
-var cardBody = document.createElement("div");
-var cardTitle = document.createElement("h5");
-var cardText = document.createElement("p");
-var startButton = document.createElement("button");
-
 // Setting attribute to the element 
-
 card.setAttribute("class","card text-center");
 cardHeader.setAttribute("class","card-header");
 cardHeader.textContent = "CODE QUIZ";
@@ -71,25 +48,38 @@ card.appendChild(cardHeader);
 card.appendChild(cardBody);
 mainElem.append(card);
 
+startButton.addEventListener("click", generateQuestion);
+
 }
 
-
 startQuiz();
-/* <div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  <div class="card-footer text-muted">
-    2 days ago
-  </div>
-</div>     */
 
+function generateQuestion(){
+    card.style.display = "none";
+    interval = setInterval(function(){
+        timerElem.textContent = totalSeconds;
+        totalSeconds--;
+
+    }, 1000);
+
+    // for (i in questions) {
+    //     var titleQuestion = document.createElement("h2");
+    //     var box = document.createElement("div");
+    //     console.log(questions[i].title);
+    //     titleQuestion.textContent = questions[i].title;
+    //     boxContainerElem.appendChild(titleQuestion);
+    //     boxContainerElem.appendChild(box);
+    //     for (j in questions[i].choices) {
+    //         var listOption = document.createElement("li");  
+    //         listOption.textContent = questions[i].choices[j];
+    //         listOption.setAttribute("data-index",j);
+    //         optionsElem.appendChild(listOption);
+    //         console.log(questions[i].choices[j]);  
+    //     }
  
-//generateQuestion();
+    //   }
+
+} 
+
 
 
